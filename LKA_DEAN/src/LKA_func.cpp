65e40a7ae1bf99c72d4f55c953af568ac1ddc5eb
WeigangZhu <Zhu_Dean@outlook.com>
2017-12-08 08:43:44 +0800

    delete the Chinese instructions

--- LKA_DEAN/src/LKA_func.cpp
+++ LKA_DEAN/src/LKA_func.cpp
@@ -5,7 +5,6 @@ int compare(const void *a, const void *b)
 	return (*(struct node*)a).distance - (*(struct node*)b).distance;
 }
 
-
 Mat imagepreprocess(Mat src)
 {
 	Mat dst, dst1, dst2, dst3,dst4,grad_x,abs_grad_x;
@@ -13,16 +12,14 @@ Mat imagepreprocess(Mat src)
 	medianBlur(dst, dst1, 3);
 	GaussianBlur(dst1, dst2, Size(3, 3), 0.5, 0.5);
 	Canny(dst2, dst3, 50, 160);
-	threshold(dst3, dst4, 155, 255, CV_THRESH_BINARY);//阈值处理转换成二值图像
+	threshold(dst3, dst4, 155, 255, CV_THRESH_BINARY);
 	return(dst4);
 }
-
 Mat ROI_Operation(Mat src)
 {
 	Mat cutimage = src(Range(20, 180), Range::all());
 	return (cutimage);
 }
-
 void linesSeparation(vector<Vec4i>lines, double kL[100], double kR[100], double bL[100], double bR[100], Vec4i lL[100], Vec4i lR[100],int &countkL, int &countkR)
 {
 	int countbL = 0, countbR = 0, m = 0, n = 0;
@@ -31,26 +28,24 @@ void linesSeparation(vector<Vec4i>lines, double kL[100], double kR[100], double 
 		Vec4i l = lines[i];
 		if (abs(l[0] - l[2])>0)
 		{
-			double k = (l[1] - l[3])*1.0 / (l[0] - l[2]);//计算斜率
+			double k = (l[1] - l[3])*1.0 / (l[0] - l[2]);
 			if (k < 0 && abs(k)>0.2&&abs(k)<2.0)
 			{
 				kL[countkL++] = k;
 				bL[countbL++] = l[1] - k*l[0];
 				lL[n++] = l;
-				cout << "kL=" << kL[countkL-1] << '\n';
+				//cout << "kL=" << kL[countkL-1] << '\n';
 			}
 			if (k>0.2&&k<2.0)
 			{
 				kR[countkR++] = k;
 				bR[countbR++] = l[1] - k*l[0];
 				lR[m++] = l;
-				cout << "kR=" << kR[countkR-1] << '\n';
+				//cout << "kR=" << kR[countkR-1] << '\n';
 			}
 		}
 	}
 }
-
-
 void distancesort(struct node d[100], double k[], double b[], int count)
 {
 	for (int j = 0; j < count; j++)
@@ -59,12 +54,11 @@ void distancesort(struct node d[100], double k[], double b[], int count)
 		d[j].num = j;
 	}
 }
-
 double LinesDistance(double k,double b)
 {
-	Point_<float> point_1, point_2, point_3,point_4;	//存放车道线上两点的世界坐标值；
-	double distance;									//距离
-	Vec4i lines;										//取图像平面车道线上两点；
+	Point_<float> point_1, point_2, point_3,point_4;	
+	double distance;									
+	Vec4i lines;										
 	lines[0] = 200.0, lines[1] = k*lines[0] + b;
 	lines[2] = 150.0, lines[3] = k*lines[2] + b;
 	point_1.x = h*(f_y*cos(alpha) - (lines[1] + 20 - v_0)*sin(alpha))*1.0 / (f_y*sin(alpha) + (lines[1] + 20 - v_0)*cos(alpha));
@@ -75,22 +69,21 @@ double LinesDistance(double k,double b)
 	point_3.x = h*(f_y*cos(alpha) - (UV_1.y - v_0)*sin(alpha))*1.0 / (f_y*sin(alpha) + (UV_1.y - v_0)*cos(alpha));
 	point_3.y = f_y / f_x*h*(u_0 - UV_1.x)*1.0 / (f_y*sin(alpha) + (UV_1.y- v_0)*cos(alpha));
 	float K, B,D,beta;
-	K = (point_2.y - point_1.y)*1.0 / (point_2.x - point_1.x);//计算世界坐标系下车道线直线斜率；
-	if (K > 0)												  //计算偏离角；
+	K = (point_2.y - point_1.y)*1.0 / (point_2.x - point_1.x);
+	if (K > 0)												  
 	{
 		beta = cvFastArctan(K, 1);
 	}
 	else
 		beta = cvFastArctan(abs(K), 1);
-	cout << "偏离角度：" << beta << '\n';
+	//cout << "Æ«ÀëœÇ¶È£º" << beta << '\n';
 	B = point_2.y - K*point_2.x;
 	D = abs(K*point_3.x - point_3.y + B)*1.0 / sqrt(1 + K*K);
 	return D;
 }
-
 void predict_point(Point &p1, Point &p2, Point &p3, Point &p4, float k, float b, float shift, float y1, float y2)
 {
-	//shift:表示平移的量；k:预测直线斜率；b:预测直线截距；y1:求与y=y1的交点；y2:求y=y2的交点
+	
 	p1.x = (y2 - b)*1.0 / k - shift;
 	p1.y = y2;
 
